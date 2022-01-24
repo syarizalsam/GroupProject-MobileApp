28f962150fe2356094f8fa8bf82aefadc7b6b0b9
syarizalsam <69450683+syarizalsam@users.noreply.github.com>
2022-01-24 14:04:23 +0800

    Add files via upload

--- components/listOrg.js
+++ components/listOrg.js
@@ -93,7 +93,7 @@ export default function ListOrg() {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#EEEEFF',
       alignItems: 'center',
       justifyContent: 'center',
     },
